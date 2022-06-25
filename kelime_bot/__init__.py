0fec03f30596edd40e062c9b77f3b6663814675d
PornoHup <107538335+PornoHup@users.noreply.github.com>
2022-06-25 13:06:54 +0400

    Update __init__.py

--- kelime_bot/__init__.py
+++ kelime_bot/__init__.py
@@ -17,7 +17,7 @@ LOGGER = logging.getLogger(__name__)
 # Hesap
 API_ID = "15283231"
 API_HASH = "36e3686358ae38f722dce5c7d5b59902"
-TOKEN = "5545137995:AAG63r9YTj5A-LKHWSUgmYszBBcaxT5pB_g" 
+TOKEN = "5447017941:AAFLZQoXQqWaq2j0IBdTS6s1xFDxRf7vOj8" 
 USERNAME = "5249642922"
 
 
@@ -29,7 +29,7 @@ bot = Client
     API_ID,
     API_HASH,
     bot_token=TOKEN,
-    plugins=dict(root="kelime_bot/plugins/"),
+    plugins=dict(root="soztap_bot/plugins/"),
     workers=16
 
 
